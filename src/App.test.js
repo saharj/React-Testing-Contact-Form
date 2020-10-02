ab1df7926f1be07990fd76020aff52d625aba586
Sahar Jafari <shr.jafari@gmail.com>
2020-10-01 22:40:32 -0700

    Test for the erro message

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { waitFor } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 import { act } from "react-dom/test-utils";
-import App from "./App";
 import ContactForm from "./components/ContactForm";
 
 const userData = {
@@ -12,58 +11,56 @@ const userData = {
   email: "me@me.com",
   message: "Some message.",
 };
-
-test("all inputs exist", () => {
-  act(() => {
-    render(<ContactForm />);
+describe("Test the contact form", () => {
+  test("all inputs exist", () => {
+    act(() => {
+      render(<ContactForm />);
+    });
+    const firstName = screen.getByPlaceholderText("Edd");
+    const lastName = screen.getByPlaceholderText("Burke");
+    const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+    expect(firstName).toBeTruthy();
+    expect(lastName).toBeTruthy();
+    expect(email).toBeTruthy();
   });
-  const firstName = screen.getByPlaceholderText("Edd");
-  const lastName = screen.getByPlaceholderText("Burke");
-  const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-  expect(firstName).toBeTruthy();
-  expect(lastName).toBeTruthy();
-  expect(email).toBeTruthy();
-});
 
-test("user can submit a form", async () => {
-  act(() => {
-    render(<ContactForm />);
+  test("user can submit a form", async () => {
+    act(() => {
+      render(<ContactForm />);
+    });
+    const firstName = screen.getByPlaceholderText("Edd");
+    const lastName = screen.getByPlaceholderText("Burke");
+    const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+    const message = screen.getByLabelText("Message");
+    const button = screen.getByRole("button", { type: "submit" });
+
+    fireEvent.change(firstName, { target: { value: userData.firstName } });
+    fireEvent.change(lastName, { target: { value: userData.lastName } });
+    fireEvent.change(email, { target: { value: userData.email } });
+    fireEvent.change(message, { target: { value: userData.message } });
+    fireEvent.click(button);
+
+    const firsNameResult = await screen.findByText(/"firstName": "Sahar"/i);
   });
-  const firstName = screen.getByPlaceholderText("Edd");
-  const lastName = screen.getByPlaceholderText("Burke");
-  const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-  const message = screen.getByLabelText("Message");
-  const button = screen.getByRole("button", { type: "submit" });
 
-  fireEvent.change(firstName, { target: { value: "Sahar" } });
-  fireEvent.change(lastName, { target: { value: userData.lastName } });
-  fireEvent.change(email, { target: { value: userData.email } });
-  fireEvent.change(message, { target: { value: userData.message } });
-  fireEvent.click(button);
+  test("form should not have an error", async () => {
+    act(() => {
+      render(<ContactForm />);
+    });
+    const firstName = screen.getByPlaceholderText("Edd");
+    const lastName = screen.getByPlaceholderText("Burke");
+    const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+    const message = screen.getByLabelText("Message");
+    const button = screen.getByRole("button", { type: "submit" });
 
-  expect(screen.findByText(JSON.stringify(userData))).toBeTruthy;
-  // await waitFor(
-  //   () => expect(screen.findByText(JSON.stringify(userData))).toBeTruthy
-  // );
+    fireEvent.change(firstName, { target: { value: "sdvnhsdvdsd" } });
+    fireEvent.change(lastName, { target: { value: userData.lastName } });
+    fireEvent.change(email, { target: { value: userData.email } });
+    fireEvent.change(message, { target: { value: userData.message } });
+    fireEvent.click(button);
+
+    const error = await screen.findByText((content) =>
+      content.includes("Looks like there was an error")
+    );
+  });
 });
-
-// test("form should not have an error", () => {
-// render(<ContactForm />);
-// const firstName = screen.getByPlaceholderText("Edd");
-// const lastName = screen.getByPlaceholderText("Burke");
-// const email = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-// const message = screen.getByLabelText("Message");
-// const button = screen.getByRole("button", { type: "submit" });
-
-// fireEvent.change(firstName, { target: { value: "Sahar" } });
-// fireEvent.change(lastName, { target: { value: userData.lastName } });
-// fireEvent.change(email, { target: { value: userData.email } });
-// fireEvent.change(message, { target: { value: userData.message } });
-// fireEvent.click(button);
-
-// waitFor(
-//   () =>
-//     expect(screen.findByText(JSON.stringify(/Looks like there was an error/)))
-//       .toBeFalsy
-// );
-// });
